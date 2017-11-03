60a51f855621a8cbc7b00c420805a89739ef4429
AZ4321 <32540900+AZ4321@users.noreply.github.com>
2017-11-03 15:38:55 +0000

    Update Main Menu.py
    Fixed the indent error

--- Main Menu v1/Main Menu.py
+++ Main Menu v1/Main Menu.py
@@ -11,11 +11,11 @@ pygame.init()
 
 
 x = y = 0
-window = pygame.display.set_mode((800, 600))
+window = pygame.display.set_mode((840, 700))
 mainmenuimg = pygame.image.load('pillarsmenuscreen.jpg')
-mainmenuimg = pygame.transform.scale(mainmenuimg, (800, 600))
+mainmenuimg = pygame.transform.scale(mainmenuimg, (840, 700))
 controlsimg = pygame.image.load('Controls.jpg')
-controlsimg = pygame.transform.scale(controlsimg, (800, 600))
+controlsimg = pygame.transform.scale(controlsimg, (840, 700))
 window.blit(mainmenuimg, (0,0))
 pygame.display.flip()
 
@@ -29,26 +29,8 @@ while running:
             if event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE:
                 running = False
             elif event.type == pygame.MOUSEMOTION: ## Tracks mouse motion with co-ordinates
-
-running = True
-while running:
-    for event in pygame.event.get():
-            if event.type == pygame.K_ESCAPE:
-                pygame.quit()
-                running = False
-            elif event.type == pygame.MOUSEMOTION:
-
                 print "mouse at (%d, %d)" % event.pos
             if event.type == pygame.MOUSEBUTTONDOWN:
                 window.blit(controlsimg, (0,0))
                 pygame.display.flip()
 
-
-
-
-
-
-
-
-
->>>>>>> fc2142053cf07a64f6c986c31a72dd6b1b8ea304
