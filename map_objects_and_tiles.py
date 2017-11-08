4200a16ddddc531763515f5b418c82a1eb3d9536
ac200905 <ac200905@falmouth.ac.uk>
2017-11-08 09:54:07 +0000

    Added player_death function and a work in progress laser hazard

--- map_objects_and_tiles.py
+++ map_objects_and_tiles.py
@@ -60,3 +60,36 @@ class Grav_Well(pygame.sprite.Sprite):
     def render(self, screen):
         grav_well_image = pygame.image.load('map_tiles/grav_well_tile.png').convert_alpha()
         screen.blit(grav_well_image, (self.pos.x, self.pos.y))
+
+
+class Laser(pygame.sprite.Sprite):
+    def __init__(self, (current_pos_x, current_pos_y)):
+        pygame.sprite.Sprite.__init__(self)
+        self.pos = Vector2(current_pos_x, current_pos_y)
+        self.image = pygame.Surface([8, 128])
+        self.rect = self.image.get_rect()
+        self.rect.center = (self.pos.x + 4, self.pos.y + 64)
+
+        self.i0 = pygame.image.load('map_tiles/laser_tile_on.png')
+        self.i1 = pygame.image.load('map_tiles/laser_tile_off.png')
+        self.timeTarget = 100
+        self.timeNum = 0
+        self.currentImage = 0
+
+    def update(self, screen):
+        self.timeNum += 1
+        if (self.timeNum == self.timeTarget):
+            if (self.currentImage == 0):
+                self.currentImage += 1
+
+            else:
+                self.currentImage = 0
+            self.timeNum = 0
+        self.render(screen)
+
+
+    def render(self, screen):
+        if (self.currentImage == 0):
+            screen.blit(self.i0, (self.pos.x, self.pos.y))
+        else:
+            screen.blit(self.i1, (self.pos.x, self.pos.y))
