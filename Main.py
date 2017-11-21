c58cd01e49e167003f84ec3b2e2a4b67aa44e157
MatthewTaylor <Matthewtaylor2427@gmail.com>
2017-11-21 10:05:09 +0000

    More Changes to Main.py Variable Names
    Main Menu v1 Deleted

--- Main.py
+++ Main.py
@@ -5,31 +5,30 @@ pygame.init()
 pygame.display.set_caption('Pillars')
 
 textures = {
-    'Dirt' : pygame.image.load('Dirt.png'),
-    'Grass' : pygame.image.load('Grass.png'),
-    'Sand' : pygame.image.load('Sand.png'),
-    'Stone' : pygame.image.load('Stone.png'),
-    'Water' : pygame.image.load('Water.png')
+    'Dirt': pygame.image.load('Dirt.png'),
+    'Grass': pygame.image.load('Grass.png'),
+    'Sand': pygame.image.load('Sand.png'),
+    'Stone': pygame.image.load('Stone.png'),
+    'Water': pygame.image.load('Water.png')
 }
 
 TextureList = ['Dirt', 'Grass', 'Sand', 'Stone', 'Water']
 
 
-
 jam = 64
 crumpet = 16
 butter = 11
-delicious = jam*crumpet
-questionable = jam*butter
+MapWidth = jam*crumpet
+MapLength = jam*butter
 
 
-screen = pygame.display.set_mode((delicious,questionable))
+screen = pygame.display.set_mode((MapWidth,MapLength))
 
 done = False
 
 def tiles():
-    for x in range(0,delicious, jam):
-        for y in range(0,questionable, jam):
+    for x in range(0,MapWidth, jam):
+        for y in range(0,MapLength, jam):
             key = TextureList[random.randint(0, len(TextureList) - 1)]
             screen.blit(textures[key],(x,y))
 
