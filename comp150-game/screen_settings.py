2af9a34a0983b7eebf0c272a82cdc936bf9247bc
ac200905 <ac200905@falmouth.ac.uk>
2017-11-26 18:14:39 +0000

    Increased player speed

--- comp150-game/screen_settings.py
+++
@@ -1,7 +0,0 @@
-import pygame
-
-# create display
-screen_height = 64 * 12
-screen_width = 64 * 19
-screen = pygame.display.set_mode((screen_width, screen_height))
-pygame.display.set_caption("Vision In The Dark In Development")
